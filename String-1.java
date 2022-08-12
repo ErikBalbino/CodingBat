035c229572a11f1fd81a9be288d45297ff68e976
ErikBalbino <marcioerik07@gmail.com>
2022-08-11 22:22:05 -0300

    add exercises string python

--- String-1.java
+++ String-1.java
@@ -1,59 +1,59 @@
-//helloName
-//Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
-//helloName("Bob") → "Hello Bob!"
-//helloName("Alice") → "Hello Alice!"
-//helloName("X") → "Hello X!"
+// helloName
+// Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+// helloName("Bob") → "Hello Bob!"
+// helloName("Alice") → "Hello Alice!"
+// helloName("X") → "Hello X!"
 
 public String helloName(String name) {
   return ("Hello "+name+"!");
 }
 
-//makeAbba
-//Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
-//makeAbba("Hi", "Bye") → "HiByeByeHi"
-//makeAbba("Yo", "Alice") → "YoAliceAliceYo"
-//makeAbba("What", "Up") → "WhatUpUpWhat"
+// makeAbba
+// Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+// makeAbba("Hi", "Bye") → "HiByeByeHi"
+// makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+// makeAbba("What", "Up") → "WhatUpUpWhat"
 
 public String makeAbba(String a, String b) {
   return (a+b+b+a);
 }
 
-//makeTags
-//The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
-//makeTags("i", "Yay") → "<i>Yay</i>"
-//makeTags("i", "Hello") → "<i>Hello</i>"
-//makeTags("cite", "Yay") → "<cite>Yay</cite>"
+// makeTags
+// The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+// makeTags("i", "Yay") → "<i>Yay</i>"
+// makeTags("i", "Hello") → "<i>Hello</i>"
+// makeTags("cite", "Yay") → "<cite>Yay</cite>"
 
 public String makeTags(String tag, String word) {
   return ("<"+tag+">"+word+"</"+tag+">");
 }
 
-//makeOutWord
-//Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>". Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
-//makeOutWord("<<>>", "Yay") → "<<Yay>>"
-//makeOutWord("<<>>", "WooHoo") → "<<WooHoo>>"
-//makeOutWord("[[]]", "word") → "[[word]]"
+// makeOutWord
+// Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>". Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+// makeOutWord("<<>>", "Yay") → "<<Yay>>"
+// makeOutWord("<<>>", "WooHoo") → "<<WooHoo>>"
+// makeOutWord("[[]]", "word") → "[[word]]"
 
 public String makeOutWord(String out, String word) {
   return (out.substring(0, 2)+word+out.substring(2, 4));
 }
 
-//extraEnd
-//Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
-//extraEnd("Hello") → "lololo"
-//extraEnd("ab") → "ababab"
-//extraEnd("Hi") → "HiHiHi"
+// extraEnd
+// Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+// extraEnd("Hello") → "lololo"
+// extraEnd("ab") → "ababab"
+// extraEnd("Hi") → "HiHiHi"
 
 public String extraEnd(String str) {
   String last = str.substring(str.length()-2, str.length());
   return (last+last+last);
 }
 
-//firstTwo
-//Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "". Note that str.length() returns the length of a string.
-//firstTwo("Hello") → "He"
-//firstTwo("abcdefg") → "ab"
-//firstTwo("ab") → "ab"
+// firstTwo
+// Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "". Note that str.length() returns the length of a string.
+// firstTwo("Hello") → "He"
+// firstTwo("abcdefg") → "ab"
+// firstTwo("ab") → "ab"
 
 public String firstTwo(String str) {
   String last;
@@ -65,38 +65,33 @@ public String firstTwo(String str) {
   return last;
 }
 
-//firstHalf
-//
-//Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
-//firstHalf("WooHoo") → "Woo"
-//firstHalf("HelloThere") → "Hello"
-//firstHalf("abcdef") → "abc"
+// firstHalf
+// Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+// firstHalf("WooHoo") → "Woo"
+// firstHalf("HelloThere") → "Hello"
+// firstHalf("abcdef") → "abc"
 
 public String firstHalf(String str) {
   return str.substring(0, str.length()/2);
 }
 
-//withoutEnd
-//
-//Given a string, return a version without the first and last char, so "Hello" //yields "ell". The string length will be at least 2.
-//
-//
-//withoutEnd("Hello") → "ell"
-//withoutEnd("java") → "av"
-//withoutEnd("coding") → "odin"
+// withoutEnd
+// Given a string, return a version without the first and last char, so "Hello" //yields "ell". The string length will be at least 2.
+// 
+// withoutEnd("Hello") → "ell"
+// withoutEnd("java") → "av"
+// withoutEnd("coding") → "odin"
 
 public String withoutEnd(String str) {
   return str.substring(1, str.length()-1);
 }
 
-//comboString
+// comboString
+// Given 2 strings, a and b, return a string of the form short+long+short, with the //shorter string on the outside and the longer string on the inside. The strings //will not be the same length, but they may be empty (length 0).
 //
-//Given 2 strings, a and b, return a string of the form short+long+short, with the //shorter string on the outside and the longer string on the inside. The strings //will not be the same length, but they may be empty (length 0).
-//
-//
-//comboString("Hello", "hi") → "hiHellohi"
-//comboString("hi", "Hello") → "hiHellohi"
-//comboString("aaa", "b") → "baaab"
+// comboString("Hello", "hi") → "hiHellohi"
+// comboString("hi", "Hello") → "hiHellohi"
+// comboString("aaa", "b") → "baaab"
 
 public String comboString(String a, String b) {
   if (a.length() >= b.length()) {
@@ -106,53 +101,45 @@ public String comboString(String a, String b) {
   }
 }
 
-//nonStart
-//
-//Given 2 strings, return their concatenation, except omit the first char of each. //The strings will be at least length 1.
-//
-//
-//nonStart("Hello", "There") → "ellohere"
-//nonStart("java", "code") → "avaode"
-//nonStart("shotl", "java") → "hotlava"
+// nonStart
+// Given 2 strings, return their concatenation, except omit the first char of each. //The strings will be at least length 1.
+// 
+// nonStart("Hello", "There") → "ellohere"
+// nonStart("java", "code") → "avaode"
+// nonStart("shotl", "java") → "hotlava"
 
 public String nonStart(String a, String b) {
   return a.substring(1, a.length())+b.substring(1, b.length());
 }
 
-//left2
-//
-//Given a string, return a "rotated left 2" version where the first 2 chars are //moved to the end. The string length will be at least 2.
-//
-//
-//left2("Hello") → "lloHe"
-//left2("java") → "vaja"
-//left2("Hi") → "Hi"
+// left2
+// Given a string, return a "rotated left 2" version where the first 2 chars are //moved to the end. The string length will be at least 2.
+// 
+// left2("Hello") → "lloHe"
+// left2("java") → "vaja"
+// left2("Hi") → "Hi"
 
 public String left2(String str) {
   return str.substring(2)+str.substring(0, 2);
 }
 
-//right2
-//
-//Given a string, return a "rotated right 2" version where the last 2 chars are //moved to the start. The string length will be at least 2.
-//
-//
-//right2("Hello") → "loHel"
-//right2("java") → "vaja"
-//right2("Hi") → "Hi"
+// right2
+// Given a string, return a "rotated right 2" version where the last 2 chars are //moved to the start. The string length will be at least 2.
+// 
+// right2("Hello") → "loHel"
+// right2("java") → "vaja"
+// right2("Hi") → "Hi"
 
 public String right2(String str) {
   return str.substring(str.length()-2)+str.substring(0, str.length()-2);
 }
 
-//theEnd
-//Given a string, return a string length 1 from its front, unless front is false, 
-//in which case return a string length 1 from its back. The string will be 
-//non-empty.
-//
-//theEnd("Hello", true) → "H"
-//theEnd("Hello", false) → "o"
-//theEnd("oh", true) → "o"
+// theEnd
+// Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+// 
+// theEnd("Hello", true) → "H"
+// theEnd("Hello", false) → "o"
+// theEnd("oh", true) → "o"
 
 public String theEnd(String str, boolean front) {
   if (front) {
@@ -162,13 +149,13 @@ public String theEnd(String str, boolean front) {
   }
 }
 
-//withouEnd2
-//Given a string, return a version without both the first and last char of the 
-//string. The string may be any length, including 0.
-//
-//withouEnd2("Hello") → "ell"
-//withouEnd2("abc") → "b"
-//withouEnd2("ab") → ""
+// withouEnd2
+// Given a string, return a version without both the first and last char of the 
+// string. The string may be any length, including 0.
+// 
+// withouEnd2("Hello") → "ell"
+// withouEnd2("abc") → "b"
+// withouEnd2("ab") → ""
 
 public String withouEnd2(String str) {
   if (str.length() <= 2) {
@@ -178,13 +165,13 @@ public String withouEnd2(String str) {
   }
 }
 
-//middleTwo
-//Given a string of even length, return a string made of the middle two chars, so 
-//the string "string" yields "ri". The string length will be at least 2.
+// middleTwo
+// Given a string of even length, return a string made of the middle two chars, so 
+// the string "string" yields "ri". The string length will be at least 2.
 //
-//middleTwo("string") → "ri"
-//middleTwo("code") → "od"
-//middleTwo("Practice") → "ct"
+// middleTwo("string") → "ri"
+// middleTwo("code") → "od"
+// middleTwo("Practice") → "ct"
 
 public String middleTwo(String str) {
   if (str.length() > 2) {
@@ -194,12 +181,12 @@ public String middleTwo(String str) {
   }
 }
 
-//endsLy
-//Given a string, return true if it ends in "ly".
-//
-//endsLy("oddly") → true
-//endsLy("y") → false
-//endsLy("oddy") → false
+// endsLy
+// Given a string, return true if it ends in "ly".
+// 
+// endsLy("oddly") → true
+// endsLy("y") → false
+// endsLy("oddy") → false
 
 public boolean endsLy(String str) {
   if (str.length() >= 2) {
---
+++ String-1.py
@@ -0,0 +1,70 @@
+# hello_name
+# Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+# 
+# hello_name('Bob') → 'Hello Bob!'
+# hello_name('Alice') → 'Hello Alice!'
+# hello_name('X') → 'Hello X!'
+
+def hello_name(name):
+  return "Hello "+name+"!"
+
+# make_abba
+# Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+# 
+# make_abba('Hi', 'Bye') → 'HiByeByeHi'
+# make_abba('Yo', 'Alice') → 'YoAliceAliceYo'
+# make_abba('What', 'Up') → 'WhatUpUpWhat'
+
+def make_abba(a, b):
+  return a+b+b+a
+
+# make_tags
+# The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+# 
+# make_tags('i', 'Yay') → '<i>Yay</i>'
+# make_tags('i', 'Hello') → '<i>Hello</i>'
+# make_tags('cite', 'Yay') → '<cite>Yay</cite>'
+
+def make_tags(tag, word):
+  return '<'+tag+'>'+word+'</'+tag+'>'
+
+# make_out_word
+# Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>".
+# 
+# make_out_word('<<>>', 'Yay') → '<<Yay>>'
+# make_out_word('<<>>', 'WooHoo') → '<<WooHoo>>'
+# make_out_word('[[]]', 'word') → '[[word]]'
+
+def make_out_word(out, word):
+  size = len(out)
+  return out[0:size/2]+word+out[size/2:]
+
+# extra_end
+# Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+# 
+# extra_end('Hello') → 'lololo'
+# extra_end('ab') → 'ababab'
+# extra_end('Hi') → 'HiHiHi'
+
+def extra_end(str):
+  return str[-2:]*3
+
+# first_two
+# Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "".
+# 
+# first_two('Hello') → 'He'
+# first_two('abcdefg') → 'ab'
+# first_two('ab') → 'ab'
+
+def first_two(str):
+  return str if len(str) <= 2 else str[0:2]
+
+# first_half
+# Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+# first_half('WooHoo') → 'Woo'
+# first_half('HelloThere') → 'Hello'
+# first_half('abcdef') → 'abc'
+
+def first_half(str):
+  return str[0:len(str)/2]
