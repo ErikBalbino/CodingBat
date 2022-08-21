bdad9ac6517f9bd7548e13ee98eeeca950e70cb4
ErikBalbino <marcioerik07@gmail.com>
2022-08-21 18:25:21 -0300

    add new exercises file java

---
+++ Warmup-1.java
@@ -0,0 +1,32 @@
+// sleepIn
+/* The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+sleepIn(false, false) → true
+sleepIn(true, false) → false
+sleepIn(false, true) → true */
+
+public boolean sleepIn(boolean weekday, boolean vacation) {
+    return !(weekday && !vacation);
+  }
+
+  // monkeyTrouble
+  /* We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+
+monkeyTrouble(true, true) → true
+monkeyTrouble(false, false) → true
+monkeyTrouble(true, false) → false */
+
+public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+    return (aSmile==bSmile);
+  }
+
+// sumDouble
+/* Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+sumDouble(1, 2) → 3
+sumDouble(3, 2) → 5
+sumDouble(2, 2) → 8 */
+
+public int sumDouble(int a, int b) {
+    if (a==b) {return (a+b)*2;} else {return a+b;}
+  }
