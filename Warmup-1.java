34105d4487315b9aa80dfa9d60a52c5b183024e1
ErikBalbino <marcioerik07@gmail.com>
2022-08-22 23:47:16 -0300

    add new exercises to warmup java

--- Warmup-1.java
+++ Warmup-1.java
@@ -30,3 +30,47 @@ sumDouble(2, 2) → 8 */
 public int sumDouble(int a, int b) {
     if (a==b) {return (a+b)*2;} else {return a+b;}
   }
+
+// diff21
+/* Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+diff21(19) → 2
+diff21(10) → 11
+diff21(21) → 0 */
+
+public int diff21(int n) {
+  if (n<=21) {return 21-n;} else {return (n-21)*2;}
+}
+
+// parrotTrouble
+/* We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+
+parrotTrouble(true, 6) → true
+parrotTrouble(true, 7) → false
+parrotTrouble(false, 6) → false */
+
+public boolean parrotTrouble(boolean talking, int hour) {
+  return (talking && (hour<7 || hour>20));
+}
+
+// makes10
+/* Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+makes10(9, 10) → true
+makes10(9, 9) → false
+makes10(1, 9) → true */
+
+public boolean makes10(int a, int b) {
+  return (a==10 || b==10 || a+b==10);
+}
+
+// nearHundred
+/* Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+
+nearHundred(93) → true
+nearHundred(90) → true
+nearHundred(89) → false */
+
+public boolean nearHundred(int n) {
+  return (!(n<0) && (n>89 && n<111 || n>189 && n<211));
+}
