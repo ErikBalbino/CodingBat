60ccf172080c0726c0332a003869967ffd6cf53c
ErikBalbino <marcioerik07@gmail.com>
2022-08-24 21:23:44 -0300

    add new exercises

--- List-1.py
+++ List-1.py
@@ -98,3 +98,25 @@ def sum2(nums):
   else:
     n = [nums[0], nums[1]]
     return sum(n)
+
+# middle_way
+
+# Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+#
+# middle_way([1, 2, 3], [4, 5, 6]) → [2, 5]
+# middle_way([7, 7, 7], [3, 8, 0]) → [7, 8]
+# middle_way([5, 2, 9], [1, 4, 5]) → [2, 4]
+
+def middle_way(a, b):
+  return [a[1], b[1]]
+
+# make_ends
+
+# Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+#
+# make_ends([1, 2, 3]) → [1, 3]
+# make_ends([1, 2, 3, 4]) → [1, 4]
+# make_ends([7, 4, 6, 2]) → [7, 2]
+
+def make_ends(nums):
+  return [nums[0], nums[-1]]
