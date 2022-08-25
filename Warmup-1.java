7be88d8f3ec20feb6ae99aa388ad7beaf4569409
ErikBalbino <marcioerik07@gmail.com>
2022-08-24 21:15:08 -0300

    add new exercises

--- Warmup-1.java
+++ Warmup-1.java
@@ -74,3 +74,131 @@ nearHundred(89) → false */
 public boolean nearHundred(int n) {
   return (!(n<0) && (n>89 && n<111 || n>189 && n<211));
 }
+
+// posNeg
+/* Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+
+posNeg(1, -1, false) → true
+posNeg(-1, 1, false) → true
+posNeg(-4, -5, true) → true */
+
+public boolean posNeg(int a, int b, boolean negative) {
+  if (negative) {return a<0 && b<0;} else {return (a<0 || b<0) && !(a<0 && b<0);}
+}
+
+// notString
+
+/* Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
+
+notString("candy") → "not candy"
+notString("x") → "not x"
+notString("not bad") → "not bad" */
+
+public String notString(String str) {
+  if (str.indexOf("not") == 0) {return str;} else {return "not "+str;}
+}
+
+// missingChar
+
+/* Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+
+missingChar("kitten", 1) → "ktten"
+missingChar("kitten", 0) → "itten"
+missingChar("kitten", 4) → "kittn" */
+
+public String missingChar(String str, int n) {
+  return str.replace(str.substring(n, n+1), "");
+}
+
+// frontBack
+
+/* Given a string, return a new string where the first and last chars have been exchanged.
+
+frontBack("code") → "eodc"
+frontBack("a") → "a"
+frontBack("ab") → "ba" */
+
+public String frontBack(String str) {
+  if (str.length()>1) {
+    String inicio = str.substring(0, 1);
+    String meio = str.substring(1, str.length()-1);
+    String fim = str.substring(str.length()-1, str.length());
+    return fim+meio+inicio;
+  } else {return str;}
+}
+
+// front3
+
+/* Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+
+front3("Java") → "JavJavJav"
+front3("Chocolate") → "ChoChoCho"
+front3("abc") → "abcabcabc" */
+
+public String front3(String str) {
+  if (str.length()>3) {return str.substring(0, 3)+str.substring(0, 3)+str.substring(0, 3);} else {return str+str+str;}
+}
+
+// backAround
+
+/* Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". The original string will be length 1 or more.
+
+backAround("cat") → "tcatt"
+backAround("Hello") → "oHelloo"
+backAround("a") → "aaa" */
+
+public String backAround(String str) {
+  String last = str.substring(str.length()-1, str.length());
+  return last+str+last;
+}
+
+// or35
+
+/* Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator -- see Introduction to Mod
+
+or35(3) → true
+or35(10) → true
+or35(8) → false */
+
+public boolean or35(int n) {
+  return (n%3)==0 || (n%5)==0;
+}
+
+// front22
+
+/* Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+
+front22("kitten") → "kikittenki"
+front22("Ha") → "HaHaHa"
+front22("abc") → "ababcab" */
+
+public String front22(String str) {
+  if (str.length()>2) {
+    String inicio = str.substring(0, 2);
+    return inicio+str+inicio;
+  } else {return str+str+str;}
+}
+
+// startHi
+
+/* Given a string, return true if the string starts with "hi" and false otherwise.
+
+startHi("hi there") → true
+startHi("hi") → true
+startHi("hello hi") → false */
+
+public boolean startHi(String str) {
+  return str.indexOf("hi")==0;
+}
+
+// icyHot
+
+/* Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+
+icyHot(120, -1) → true
+icyHot(-1, 120) → true
+icyHot(2, 120) → false */
+
+public boolean icyHot(int temp1, int temp2) {
+  return (temp1<0 && temp2>100) || (temp1>100 && temp2<0);
+}
