957676ddbc6ad62b56485fd7cbbb7ce468b11ed9
ErikBalbino <marcioerik07@gmail.com>
2022-09-03 23:19:01 -0300

    add new exercises

--- String-2.py
+++ String-2.py
@@ -33,3 +33,28 @@ def count_hi(str):
 
 def cat_dog(str):
   return (str.count('cat') == str.count('dog'))
+
+# count_code
+
+# Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+# count_code('aaacodebbb') → 1
+# count_code('codexxcode') → 2
+# count_code('cozexxcope') → 2
+
+def count_code(str):
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  c = 0
+  for x in alphabet:
+    c += str.count('co'+x+'e')
+  return c
+
+# end_other
+
+# Given two strings, return True if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). Note: s.lower() returns the lowercase version of a string.
+
+# end_other('Hiabc', 'abc') → True
+# end_other('AbC', 'HiaBc') → True
+# end_other('abc', 'abXabc') → True
+
+
